0d3c9c27c86d6b5b7c120a2ddbe710b36207093f
mmpokharanakar <mmpokharanakar@gmail.com>
2024-08-11 22:27:04 +0530

    structures

--- MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
+++ MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
@@ -36,22 +36,74 @@ variable (x y z : α)
 #check (sup_le : x ≤ z → y ≤ z → x ⊔ y ≤ z)
 
 example : x ⊓ y = y ⊓ x := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply le_inf
+    · apply inf_le_right
+    apply inf_le_left
 
 example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := by
-  sorry
+  apply le_antisymm
+  · apply le_inf
+    · trans x ⊓ y
+      apply inf_le_left
+      apply inf_le_left
+    apply le_inf
+    · trans x ⊓ y
+      apply inf_le_left
+      apply inf_le_right
+    apply inf_le_right
+  apply le_inf
+  · apply le_inf
+    · apply inf_le_left
+    trans y ⊓ z
+    apply inf_le_right
+    apply inf_le_left
+  trans y ⊓ z
+  apply inf_le_right
+  apply inf_le_right
 
 example : x ⊔ y = y ⊔ x := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply sup_le
+    · apply le_sup_right
+    apply le_sup_left
 
 example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := by
-  sorry
+  apply le_antisymm
+  · apply sup_le
+    · apply sup_le
+      apply le_sup_left
+      · trans y ⊔ z
+        apply le_sup_left
+        apply le_sup_right
+    trans y ⊔ z
+    apply le_sup_right
+    apply le_sup_right
+  apply sup_le
+  · trans x ⊔ y
+    apply le_sup_left
+    apply le_sup_left
+  apply sup_le
+  · trans x ⊔ y
+    apply le_sup_right
+    apply le_sup_left
+  apply le_sup_right
 
 theorem absorb1 : x ⊓ (x ⊔ y) = x := by
-  sorry
+  apply le_antisymm
+  · apply inf_le_left
+  apply le_inf
+  · apply le_refl
+  apply le_sup_left
 
 theorem absorb2 : x ⊔ x ⊓ y = x := by
-  sorry
+  apply le_antisymm
+  · apply sup_le
+    · apply le_refl
+    apply inf_le_left
+  apply le_sup_left
 
 end
 
@@ -70,10 +122,12 @@ variable {α : Type*} [Lattice α]
 variable (a b c : α)
 
 example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z) : a ⊔ b ⊓ c = (a ⊔ b) ⊓ (a ⊔ c) := by
-  sorry
+  rw [h, @inf_comm _ _ (a ⊔ b), absorb1, @inf_comm _ _ (a ⊔ b)]
+  rw [h, ← sup_assoc, @inf_comm _ _ c a, absorb2, inf_comm]
 
 example (h : ∀ x y z : α, x ⊔ y ⊓ z = (x ⊔ y) ⊓ (x ⊔ z)) : a ⊓ (b ⊔ c) = a ⊓ b ⊔ a ⊓ c := by
-  sorry
+  rw [h, @sup_comm _ _ (a ⊓ b), absorb2, @sup_comm _ _ (a ⊓ b)]
+  rw [h, ← inf_assoc, @sup_comm _ _ c a, absorb1, sup_comm]
 
 end
 
@@ -87,13 +141,27 @@ variable (a b c : R)
 #check (mul_nonneg : 0 ≤ a → 0 ≤ b → 0 ≤ a * b)
 
 example (h : a ≤ b) : 0 ≤ b - a := by
-  sorry
+  rw [← sub_self a, sub_eq_add_neg, sub_eq_add_neg]
+  rw [add_comm, add_comm b]
+  apply add_le_add_left h
 
 example (h: 0 ≤ b - a) : a ≤ b := by
-  sorry
+  rw [← add_zero a, ← sub_add_cancel b a, add_comm (b - a)]
+  apply add_le_add_left h
+
+theorem aux1 (h : a ≤ b) : 0 ≤ b - a := by
+  rw [← sub_self a, sub_eq_add_neg, sub_eq_add_neg]
+  rw [add_comm, add_comm b]
+  apply add_le_add_left h
+
+theorem aux2 (h: 0 ≤ b - a) : a ≤ b := by
+  rw [← add_zero a, ← sub_add_cancel b a, add_comm (b - a)]
+  apply add_le_add_left h
 
 example (h : a ≤ b) (h' : 0 ≤ c) : a * c ≤ b * c := by
-  sorry
+  have h1 : 0 ≤ (b - a) * c := mul_nonneg (aux1 _ _ h) h'
+  rw [sub_mul] at h1
+  exact aux2 _ _ h1
 
 end
 
@@ -106,7 +174,9 @@ variable (x y z : X)
 #check (dist_triangle x y z : dist x z ≤ dist x y + dist y z)
 
 example (x y : X) : 0 ≤ dist x y := by
-  sorry
+  have : 0 ≤ dist x y + dist y x := by
+    rw [← dist_self x]
+    apply dist_triangle
+  linarith [dist_comm x y]
 
 end
-
